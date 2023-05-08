f479c4c715657a9b096049cdf2357c01f0ddd262
tobiasz-rayski <124359533+tobiasz-rayski@users.noreply.github.com>
2023-05-08 12:00:12 +0200

    Add user and CPU selection functionality

--- script.js
+++ script.js
@@ -13,4 +13,13 @@ function roshamboGame() {
       loses: "rock",
     },
   };
+
+  const optionsArr = Object.keys(options);
+
+  let userSelect = prompt("Choose your weapon...");
+
+  let cpuSelect = optionsArr[Math.floor(Math.random() * optionsArr.length)];
+  console.log(cpuSelect);
 }
+
+roshamboGame();
