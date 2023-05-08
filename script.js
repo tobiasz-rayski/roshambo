2905d299dc75e875695896a766741cbffcf6f70f
tobiasz-rayski <124359533+tobiasz-rayski@users.noreply.github.com>
2023-05-08 11:47:37 +0200

    Create options object

---
+++ script.js
@@ -0,0 +1,16 @@
+function roshamboGame() {
+  const options = {
+    rock: {
+      wins: "scissors",
+      loses: "paper",
+    },
+    paper: {
+      wins: "rock",
+      loses: "scissors",
+    },
+    scissors: {
+      wins: "paper",
+      loses: "rock",
+    },
+  };
+}
